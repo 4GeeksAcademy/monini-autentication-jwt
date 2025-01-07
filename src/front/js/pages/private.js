bb85423fad273b5d85cb3ecb9ea32dd52eae8ef6
Monique Latil <moninilatil@gmail.com>
2025-01-07 16:36:56 +0000

    falta si el token está chimbo

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -2,6 +2,15 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const Private = () => {
+
+    const navigate = useNavigate()
+    useEffect(() => {
+        if (!localStorage.getItem("token")) {
+            navigate("/")
+        }
+        
+    }, [])
+
     return (
         <div style={{ width: '50%', margin: '0 auto' }}>
             <img src="https://static.vecteezy.com/system/resources/previews/010/925/778/non_2x/colorful-welcome-design-template-free-vector.jpg" style={{ width: '100%' }} />
